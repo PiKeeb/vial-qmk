1fe54e8b61602b88f7a430a705798c78aa92b945
PiKeeb <pikeeb@gmail.com>
2024-08-20 21:13:39 +0300

    Added comment about RGB LED (GB vs OSH version)

--- keyboards/lyra58/config.h
+++ keyboards/lyra58/config.h
@@ -1,6 +1,13 @@
 // Copyright 2024 ILWrites (@PiKeeb)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
+/* RGB LED
+ *
+ * In case you've got the PCB from the original GB and not the OSH version,
+ * please, change the "ws2812.pin" in info.json to B4.
+ *
+ */
+
 #pragma once
 
 /*
